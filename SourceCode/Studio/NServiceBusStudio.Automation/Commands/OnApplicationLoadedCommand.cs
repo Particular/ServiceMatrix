7b20d0ce6a1bdde4a7123d96d926ae438cda8c82
Ezequiel Sculli <sculli_e@hotmail.com>
2013-04-11 17:36:08 -0300

    Show Solution Builder when open new Solution

--- SourceCode/Studio/NServiceBusStudio.Automation/Commands/OnApplicationLoadedCommand.cs
+++ SourceCode/Studio/NServiceBusStudio.Automation/Commands/OnApplicationLoadedCommand.cs
@@ -7,24 +7,21 @@ using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.Composition;
 using NuPattern.Runtime;
+using NuPattern.Runtime.Shell;
+using Microsoft.VisualStudio.Shell;
 
 namespace NServiceBusStudio.Automation.Commands
 {
     [DisplayName("On Application Loaded")]
     public class OnApplicationLoadedCommand : FeatureCommand
     {
-        [Required]
-        [Import(AllowDefault = true)]
-        public IProductElement CurrentElement
-        {
-            get;
-            set;
-        }
+        [Import(typeof(SVsServiceProvider))]
+        public IServiceProvider ServiceProvider { get; set; }
 
         public override void Execute()
         {
-            //this.Viewer.ShowContent(Guid.NewGuid(), "NServiceBus Details Pane", new NServiceBusStudio.Automation.CustomSolutionBuilder.Views.DetailsPanel());
-            // this.CurrentElement.As<IApplication>().RaiseOnApplicationLoaded();
+            var ptw = this.ServiceProvider.GetService(typeof(IPackageToolWindow)) as IPackageToolWindow;
+            ptw.ShowWindow<SolutionBuilderToolWindow>();
         }
     }
 }
