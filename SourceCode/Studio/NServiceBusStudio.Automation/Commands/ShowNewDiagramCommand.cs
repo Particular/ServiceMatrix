90fa4215098e7f7973813d76a0ce95ab6c019980
Ezequiel Sculli <sculli_e@hotmail.com>
2013-08-12 12:21:44 -0300

    Added Infragistics WPF control demo

---
+++ SourceCode/Studio/NServiceBusStudio.Automation/Commands/ShowNewDiagramCommand.cs
@@ -0,0 +1,56 @@
+﻿using AbstractEndpoint;
+using NuPattern.Runtime;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NServiceBusStudio.Automation.Dialog;
+using System.Windows.Input;
+using NuPattern.Presentation;
+using NuPattern;
+
+namespace NServiceBusStudio.Automation.Commands
+{
+    public class ShowNewDiagramCommand : NuPattern.Runtime.Command
+    {
+        /// <summary>
+        /// Gets or sets the current element.
+        /// </summary>
+        [Required]
+        [Import(AllowDefault = true)]
+        public IProductElement CurrentElement
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets the Window Factory, used to create a Window Dialog.
+        /// </summary>
+        [Required]
+        [Import(AllowDefault = true)]
+        private IDialogWindowFactory WindowFactory
+        {
+            get;
+            set;
+        }
+
+        public override void Execute()
+        {
+            // Verify all [Required] and [Import]ed properties have valid values.
+            this.ValidateObject();
+
+            var window = WindowFactory.CreateDialog<NewDiagram>();
+            
+            using (new MouseCursor(Cursors.Arrow))
+            {
+                if (window.ShowDialog().Value)
+                {
+                }
+            }
+        }
+    }
+}
---
+++ SourceCode/Studio/NServiceBusStudio.Automation/Dialog/NewDiagram.xaml.cs
@@ -0,0 +1,72 @@
+﻿using NuPattern.Presentation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NServiceBusStudio.Automation.Dialog
+{
+    /// <summary>
+    /// Interaction logic for NewDiagram.xaml
+    /// </summary>
+    public partial class NewDiagram : CommonDialogWindow, IDialogWindow
+    {
+        public NewDiagram()
+        {
+            InitializeComponent();
+        }
+
+        private void CommonDialogWindow_Initialized(object sender, EventArgs e)
+        {
+            var list = new List<NodeModel>();
+
+            var node1 = new NodeModel() { Label = "Node1", Tooltip = "Tooltip 1" };
+            
+            var node2 = new NodeModel() { Label = "Node2", Tooltip = "Tooltip 2" };
+            node2.Connections.Add (new NodeConnection() { Target = node1, Weight=1 });
+
+            var node3 = new NodeModel() { Label = "Node3", Tooltip = "Tooltip 3" };
+            node3.Connections.Add(new NodeConnection() { Target = node2, Weight = 0 });
+
+            var node4 = new NodeModel() { Label = "Node4", Tooltip = "Tooltip 4" };
+            node4.Connections.Add(new NodeConnection() { Target = node2, Weight = 10 });
+            node4.Connections.Add(new NodeConnection() { Target = node3, Weight = 20 });
+
+            list.Add(node1);
+            list.Add(node2);
+            list.Add(node3);
+            list.Add(node4);
+
+            this.xnn.ItemsSource = list;
+        }
+
+
+        public class NodeModel
+        {
+            public string Label { get; set; }
+            public string Tooltip { get; set; }
+            public IList<NodeConnection> Connections { get; set; }
+
+            public NodeModel()
+            {
+                this.Connections = new List<NodeConnection>();
+            }
+        }
+
+        public class NodeConnection
+        {
+            public NodeModel Target { get; set; }
+            public int Weight { get; set; }
+        }
+    }
+}
