2a175a0afa7a477c4f26b72048713e571462a2fd
Ezequiel Sculli <sculli_e@hotmail.com>
2013-10-21 17:28:13 -0300

    Fixed #90 Delete command causes NuPattern failure

--- SourceCode/Studio/NServiceBusStudio.Automation/Model/Event.cs
+++ SourceCode/Studio/NServiceBusStudio.Automation/Model/Event.cs
@@ -16,5 +16,20 @@ namespace NServiceBusStudio
         {
             get { return this.Parent.Namespace; }
         }
+
+        partial void Initialize()
+        {
+            this.AsElement().Deleting += (sender, eventargs) =>
+            {
+                // Find Event Links to the deleted Event
+                var root = this.AsElement().Root.As<IApplication>();
+
+                var eventLinks = root.Design.Services.Service.SelectMany(s => s.Components.Component.SelectMany(c => c.Publishes.EventLinks.Where(el => el.EventReference.Value == this))).ToList();
+                eventLinks.ForEach(el => el.Delete());
+
+                var subscribedEventLinks = root.Design.Services.Service.SelectMany(s => s.Components.Component.SelectMany(c => c.Subscribes.SubscribedEventLinks.Where(el => el.EventReference.Value == this))).ToList();
+                subscribedEventLinks.ForEach(el => el.Delete());
+            };
+        }
     }
 }
