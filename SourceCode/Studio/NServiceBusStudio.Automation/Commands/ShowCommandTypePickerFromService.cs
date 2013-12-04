841e91890511d7c82574cab4cb01d069d4422acb
Ezequiel Sculli <sculli_e@hotmail.com>
2013-12-04 15:18:47 -0300

    Fixed #131, Added menus at Service Level

---
+++ SourceCode/Studio/NServiceBusStudio.Automation/Commands/ShowCommandTypePickerFromService.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.Composition;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using NuPattern;
+using NuPattern.Runtime;
+using NServiceBusStudio.Automation.TypeConverters;
+using System.Drawing.Design;
+using NServiceBusStudio.Automation.Dialog;
+using System.Windows.Input;
+using NuPattern.Diagnostics;
+using NuPattern.Presentation;
+
+namespace NServiceBusStudio.Automation.Commands
+{
+    /// <summary>
+    /// A custom command that performs some automation.
+    /// </summary>
+    [DisplayName("Show an Command Picker Dialog for Sending From Service")]
+    [Category("General")]
+    [Description("Shows a dialog where the user can choose or create a command, and adds a publish link to it.")]
+    [CLSCompliant(false)]
+    public class ShowCommandTypePickerFromService : NuPattern.Runtime.Command
+    {
+        private static readonly ITracer tracer = Tracer.Get<ShowCommandTypePickerFromService>();
+
+        /// <summary>
+        /// Gets or sets the Window Factory, used to create a Window Dialog.
+        /// </summary>
+        [Required]
+        [Import(AllowDefault = true)]
+        private IDialogWindowFactory WindowFactory
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets the current element.
+        /// </summary>
+        [Required]
+        [Import(AllowDefault = true)]
+        public IProductElement CurrentElement
+        {
+            get;
+            set;
+        }
+
+        private IService CurrentService
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Executes this commmand.
+        /// </summary>
+        /// <remarks></remarks>
+        public override void Execute()
+        {
+            this.CurrentService = this.CurrentElement.As<IService>();
+
+            // Verify all [Required] and [Import]ed properties have valid values.
+            this.ValidateObject();
+
+            var commands = CurrentService.Contract.Commands.Command;
+            var commandNames = commands.Select(e => e.InstanceName);
+
+            var picker = WindowFactory.CreateDialog<ElementPicker>() as IElementPicker;
+
+            picker.Elements = commandNames.ToList();
+            picker.Title = "Send Command";
+            picker.MasterName = "Command name";
+
+            using (new MouseCursor(Cursors.Arrow))
+            {
+                if (picker.ShowDialog().Value)
+                {
+                    var selectedElement = picker.SelectedItem;
+                    var selectedCommand = default(ICommand);
+                    if (commandNames.Contains(selectedElement))
+                    {
+                        selectedCommand = commands.FirstOrDefault(e => string.Equals(e.InstanceName, selectedElement, StringComparison.InvariantCultureIgnoreCase));
+                    }
+                    else
+                    {
+                        selectedCommand = CurrentService.Contract.Commands.CreateCommand(selectedElement);
+                    }
+                }
+            }
+            // Make initial trace statement for this command
+            //tracer.Info(
+            //    "Executing ShowElementTypePicker on current element '{0}' with AProperty '{1}'", this.CurrentElement.InstanceName, this.ElementType);
+
+            //	TODO: Use tracer.Warning() to note expected and recoverable errors
+            //	TODO: Use tracer.Verbose() to note internal execution logic decisions
+            //	TODO: Use tracer.Info() to note key results of execution
+            //	TODO: Raise exceptions for all other errors
+        }
+    }
+}
