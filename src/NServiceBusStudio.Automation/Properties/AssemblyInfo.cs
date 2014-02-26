a470b368ccaa4df7d9512266aa654227cf973a39
indualagarsamy <indu.alagarsamy@gmail.com>
2014-02-25 17:49:34 -0800

    Added gitflowversion steps to the csproj

--- src/NServiceBusStudio.Automation/Properties/AssemblyInfo.cs
+++ src/NServiceBusStudio.Automation/Properties/AssemblyInfo.cs
@@ -8,13 +8,9 @@ using Microsoft.VisualStudio.ExtensibilityHosting;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("NServiceBusStudio.Automation")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("NServiceBusStudio.Automation")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2012")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyCompany("Particular")]
+[assembly: AssemblyProduct("ServiceMatrix.Automation")]
+[assembly: AssemblyCopyright("Copyright © Particular Software 2013")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -24,19 +20,6 @@ using Microsoft.VisualStudio.ExtensibilityHosting;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("6976a1bb-5ec5-441a-be88-1349991a7c5e")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
 
 [assembly: NeutralResourcesLanguage("en")]
 
