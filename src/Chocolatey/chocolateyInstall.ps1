0ed0496add7d9360dec3bb2c8825ad49b971d488
Simon Cropp <simon.cropp@gmail.com>
2014-02-27 09:55:22 +1100

    add choco package

---
+++ src/Chocolatey/chocolateyInstall.ps1
@@ -0,0 +1,26 @@
+﻿
+$packageName = "ServiceMatrix"
+$url = "https://github.com/Particular/ServiceMatrix/releases/download/SemVer/Particular.ServiceMatrix-SemVer.exe"
+
+try {
+
+    $chocTempDir = Join-Path $env:TEMP "chocolatey"
+    $tempDir = Join-Path $chocTempDir "$packageName"
+    if (![System.IO.Directory]::Exists($tempDir)) {[System.IO.Directory]::CreateDirectory($tempDir) | Out-Null}
+    $file = Join-Path $tempDir "$($packageName)Install.exe"
+    $logFile = Join-Path $tempDir "msiexe.log"
+
+	If (Test-Path $logFile){
+		Remove-Item $logFile
+	}
+
+    Get-ChocolateyWebFile $packageName $file $url 
+	$msiArguments  ="/quiet  /L*V `"$logFile`""
+	Write-Host "Starting installer with arguments: $msiArguments";
+    Start-ChocolateyProcessAsAdmin "$msiArguments" $file -validExitCodes 0
+
+    Write-ChocolateySuccess $packageName
+} catch {
+	Write-ChocolateyFailure $packageName $($_.Exception.Message)
+	throw
+}
