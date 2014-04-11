bd134f79acb207a8db045e00b2eab33a613f1036
indualagarsamy <indu.alagarsamy@gmail.com>
2014-04-11 13:28:07 -0700

    Changed the message namespace appropriately for commands and response messages. Fixes #302

---
+++ src/ServiceMatrix.Automation.Tests/Extensions/GenerateMessageConventionsTest.cs
@@ -0,0 +1,62 @@
+﻿namespace ServiceMatrix.Automation.Tests.Extensions
+{
+    using NServiceBusStudio.Automation.Extensions;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class GenerateMessageConventionsTest
+    {
+        [Test]
+        public void GetMessageConventionsTest_WhenRootNamespaceHasAValue()
+        {
+            string rootNamespace = "MyNServiceBusSystem";
+            string applicationName = "MyNServiceBusSystem";
+            string projectNameForInternal = "InternalMessages";
+            string projectNameForContracts = "ContractEvents";
+
+            var generatedConventions = GenerateMessageConventions.GetMessageConventions(rootNamespace, applicationName, projectNameForInternal, projectNameForContracts);
+
+            var expectedConventions = @"namespace MyNServiceBusSystem
+{
+    public class MessageConventions : IWantToRunBeforeConfiguration
+    {
+        public void Init()
+        {
+            Configure.Instance
+            .DefiningCommandsAs(t => t.Namespace != null && t.Namespace.StartsWith(""MyNServiceBusSystem.InternalMessages.Commands""))
+            .DefiningEventsAs(t => t.Namespace != null && t.Namespace.StartsWith(""MyNServiceBusSystem.ContractEvents""))
+            .DefiningMessagesAs(t => t.Namespace != null && t.Namespace.StartsWith(""MyNServiceBusSystem.InternalMessages.Messages""));
+        }
+    }
+}
+";
+            Assert.AreEqual(expectedConventions, generatedConventions);
+        }
+
+        [Test]
+        public void GetMessageConventionsTest_WhenRootNamespaceIsNull()
+        {
+            string rootNamespace = null;
+            string applicationName = "MyNServiceBusSystem";
+            string projectNameForInternal = "InternalMessages";
+            string projectNameForContracts = "ContractEvents";
+
+            var generatedConventions = GenerateMessageConventions.GetMessageConventions(rootNamespace, applicationName, projectNameForInternal, projectNameForContracts);
+
+            var expectedConventions = @"{
+    public class MessageConventions : IWantToRunBeforeConfiguration
+    {
+        public void Init()
+        {
+            Configure.Instance
+            .DefiningCommandsAs(t => t.Namespace != null && t.Namespace.StartsWith(""MyNServiceBusSystem.InternalMessages.Commands""))
+            .DefiningEventsAs(t => t.Namespace != null && t.Namespace.StartsWith(""MyNServiceBusSystem.ContractEvents""))
+            .DefiningMessagesAs(t => t.Namespace != null && t.Namespace.StartsWith(""MyNServiceBusSystem.InternalMessages.Messages""));
+        }
+    }
+}
+";
+            Assert.AreEqual(expectedConventions, generatedConventions);
+        }
+    }
+}
