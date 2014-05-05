7a23b77ed4ebbe5eca0413f952f70c645cb163f6
fsimonazzi <fsimonazzi@hotmail.com>
2014-05-05 16:09:37 -0300

    change version to 2.1.0, change package name and summary

---
+++ src/ServiceMatrix/ToolkitConstants.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NServiceBusStudio
+{
+	internal static class ToolkitConstants
+	{
+		/// <summary>
+		/// E5387BC7-B3B0-4B1C-8307-6A86494FCCCF
+		/// </summary>
+		public const string VsixIdentifier = "E5387BC7-B3B0-4B1C-8307-6A86494FCCCF";
+
+		/// <summary>
+		/// ServiceMatrix
+		/// </summary>
+		public const string ToolkitName = "ServiceMatrix";
+
+		/// <summary>
+		/// Particular Software
+		/// </summary>
+		public const string Author = "Particular Software";
+
+		/// <summary>
+		/// 2.1.0
+		/// </summary>
+		public const string Version = "2.1.0";
+
+		/// <summary>
+		/// Intuitive modeling and design of distributed systems
+		/// </summary>
+		public const string Description = "Intuitive modeling and design of distributed systems";
+
+		/// <summary>
+		/// ServiceMatrix
+		/// </summary>
+		public const string ProductName = "ServiceMatrix";
+
+		/// <summary>
+		/// NServiceBusStudio
+		/// </summary>
+		public const string RootNamespace = "NServiceBusStudio";
+
+		/// <summary>
+		/// Particular.ServiceMatrix
+		/// </summary>
+		public const string AssemblyName = "Particular.ServiceMatrix";
+	}
+}
