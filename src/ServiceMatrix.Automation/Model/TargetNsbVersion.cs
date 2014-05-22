3b9c45cbe4a5565841f498756cc9215329b8e404
indualagarsamy <indu.alagarsamy@gmail.com>
2014-05-22 13:26:20 -0700

    Added a solution level property called "TargetNsbVersion" to specify if the generated solution should target version 4.0 or 5.0

---
+++ src/ServiceMatrix.Automation/Model/TargetNsbVersion.cs
@@ -0,0 +1,11 @@
+﻿namespace NServiceBusStudio.Automation.Model
+{
+    /// <summary>
+    /// List of supported NSB versions by ServiceMatrix
+    /// </summary>
+    public static class TargetNsbVersion
+    {
+        public const string Version5 = "NSB 5.0";
+        public const string Version4 = "NSB 4.0";
+    }
+}
---
+++ src/ServiceMatrix.Automation/TypeConverters/TargetNsbVersionTypeConverter.cs
@@ -0,0 +1,44 @@
+﻿namespace NServiceBusStudio.Automation.TypeConverters
+{
+    using System;
+    using System.ComponentModel;
+    using NServiceBusStudio.Automation.Model;
+
+    /// <summary>
+    /// A custom type converter for returning major versions of NServiceBus.
+    /// </summary>
+    [DisplayName("Target NSB Version Type Converter")]
+    [Category("General")]
+    [Description("Returns a list of major versions of NServiceBus")]
+    [CLSCompliant(false)]
+    public class TargetNsbVersionTypeConverter : StringConverter
+    {
+        /// <summary>
+        /// Determines whether this converter supports standard values.
+        /// </summary>
+        public override bool GetStandardValuesSupported(ITypeDescriptorContext context)
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// Determines whether this converter only allows selection of items returned by <see cref="GetStandardValues"/>.
+        /// </summary>
+        public override bool GetStandardValuesExclusive(ITypeDescriptorContext context)
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// Returns the list of string values for the enumeration
+        /// </summary>
+        public override StandardValuesCollection GetStandardValues(ITypeDescriptorContext context)
+        {
+            return new StandardValuesCollection(new[]
+            {
+                TargetNsbVersion.Version5,
+                TargetNsbVersion.Version4
+            });
+        }
+    }
+}
