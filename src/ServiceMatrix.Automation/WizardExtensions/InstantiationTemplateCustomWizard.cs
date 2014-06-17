1d8ad32ab5054d3c9d02f3bfd0d3594c32fa8aa8
fsimonazzi <fsimonazzi@hotmail.com>
2014-06-17 11:43:17 -0300

    improve the custom wizard approach

--- src/ServiceMatrix.Automation/WizardExtensions/InstantiationTemplateCustomWizard.cs
+++ src/ServiceMatrix.Automation/WizardExtensions/InstantiationTemplateCustomWizard.cs
@@ -8,6 +8,9 @@ namespace NServiceBusStudio.Automation.WizardExtensions
     {
         public override void RunStarted(object automationObject, Dictionary<string, string> replacementsDictionary, WizardRunKind runKind, object[] customParams)
         {
+            // This is done to trick NuPattern into using the v4 template when unfolding the v5 template.
+            // The template is used by NuPattern to identify the toolkit and product elements associated to the template,
+            // and there can only one template associated to the application
             base.RunStarted(automationObject, replacementsDictionary, runKind, ApplicationTemplateWorkaroundHelper.ReplaceCustomParameters(customParams));
         }
     }
--- src/ServiceMatrix.Automation/WizardExtensions/SetModelPropertiesWizard.cs
+++ src/ServiceMatrix.Automation/WizardExtensions/SetModelPropertiesWizard.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using EnvDTE;
 using Microsoft.VisualStudio.TemplateWizard;
 using NuPattern.Library;
@@ -9,12 +10,18 @@ namespace NServiceBusStudio.Automation.WizardExtensions
     public class SetModelPropertiesWizard : TemplateWizard
     {
         string targetFrameworkVersion;
+        string targetNsbVersion;
 
         public override void RunStarted(object automationObject, Dictionary<string, string> replacementsDictionary, WizardRunKind runKind, object[] customParams)
         {
             base.RunStarted(automationObject, replacementsDictionary, runKind, customParams);
 
             replacementsDictionary.TryGetValue("$targetframeworkversion$", out targetFrameworkVersion);
+            var nServiceBusVersionElement = TemplateSchema.WizardData.SelectMany(wd => wd.Elements.Where(e => e.LocalName == "NServiceBus")).FirstOrDefault();
+            if (nServiceBusVersionElement != null)
+            {
+                targetNsbVersion = nServiceBusVersionElement.GetAttribute("Version");
+            }
         }
 
         public override void ProjectFinishedGenerating(Project project)
@@ -26,7 +33,15 @@ namespace NServiceBusStudio.Automation.WizardExtensions
                 var application = UnfoldScope.Current.Automation.Owner.As<IApplication>();
                 if (application != null)
                 {
-                    application.TargetFrameworkVersion = targetFrameworkVersion;
+                    if (targetFrameworkVersion != null)
+                    {
+                        application.TargetFrameworkVersion = targetFrameworkVersion;
+                    }
+
+                    if (targetNsbVersion != null)
+                    {
+                        application.TargetNsbVersion = targetNsbVersion;
+                    }
                 }
             }
         }
