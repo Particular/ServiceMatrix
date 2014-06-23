eee7088827ada9eac790befca61d093b538c7595
fsimonazzi <fsimonazzi@hotmail.com>
2014-06-23 16:22:54 -0300

    add v5 support for transport config, endpoint config and message conventions

--- src/ServiceMatrix.Automation/Extensions/GenerateMessageConventions.cs
+++ src/ServiceMatrix.Automation/Extensions/GenerateMessageConventions.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Text;
 using AbstractEndpoint;
+using NServiceBusStudio.Automation.Model;
 using NuPattern.Runtime;
 
 namespace NServiceBusStudio.Automation.Extensions
@@ -12,7 +13,7 @@ namespace NServiceBusStudio.Automation.Extensions
             var generatedConventions = string.Empty;
             try
             {
-                var app = endpoint.Root.As<NServiceBusStudio.IApplication>();
+                var app = endpoint.Root.As<IApplication>();
 
                 var rootNameSpace = string.Empty;
                 var applicationName = app.CodeIdentifier;
@@ -24,36 +25,70 @@ namespace NServiceBusStudio.Automation.Extensions
                 {
                     rootNameSpace = project.Data.RootNamespace;
                 }
-                generatedConventions = GetMessageConventions(rootNameSpace, applicationName, projectNameForInternal, projectNameForContracts);
+
+                generatedConventions =
+                    app.TargetNsbVersion == TargetNsbVersion.Version4
+                    ? GetMessageConventionsV4(rootNameSpace, applicationName, projectNameForInternal, projectNameForContracts)
+                    : GetMessageConventionsV5(rootNameSpace, applicationName, projectNameForInternal, projectNameForContracts);
             }
             catch (Exception ex)
             {
                 //TODO: Why are we catching the exception here??
             }
+
             return generatedConventions;
         }
 
-        public static string GetMessageConventions(string rootNamespace, string applicationName, string projectNameForInternal, string projectNameForContracts)
+        public static string GetMessageConventionsV4(string rootNamespace, string applicationName, string projectNameForInternal, string projectNameForContracts)
         {
             var sb = new StringBuilder();
             if (!String.IsNullOrEmpty(rootNamespace))
-                {
-                    sb.AppendLine("namespace " + rootNamespace);
-                }
-                sb.Append(@"{
+            {
+                sb.AppendLine("namespace " + rootNamespace);
+            }
+
+            sb.Append(@"{
     public class MessageConventions : IWantToRunBeforeConfiguration
     {
         public void Init()
         {
             Configure.Instance");
-                sb.AppendLine();
-                sb.AppendLine("            .DefiningCommandsAs(t => t.Namespace != null && t.Namespace.StartsWith(\"" +
-                    applicationName + "." + projectNameForInternal + ".Commands\"))");
-                sb.AppendLine("            .DefiningEventsAs(t => t.Namespace != null && t.Namespace.StartsWith(\"" +
-                    applicationName + "." + projectNameForContracts + "\"))");
-                sb.AppendLine("            .DefiningMessagesAs(t => t.Namespace != null && t.Namespace.StartsWith(\"" +
-                    applicationName + "." + projectNameForInternal + ".Messages\"));");
-                sb.Append(@"        }
+            sb.AppendLine();
+            sb.AppendLine("            .DefiningCommandsAs(t => t.Namespace != null && t.Namespace.StartsWith(\"" +
+                applicationName + "." + projectNameForInternal + ".Commands\"))");
+            sb.AppendLine("            .DefiningEventsAs(t => t.Namespace != null && t.Namespace.StartsWith(\"" +
+                applicationName + "." + projectNameForContracts + "\"))");
+            sb.AppendLine("            .DefiningMessagesAs(t => t.Namespace != null && t.Namespace.StartsWith(\"" +
+                applicationName + "." + projectNameForInternal + ".Messages\"));");
+            sb.Append(@"        }
+    }
+}
+");
+            return sb.ToString();
+        }
+
+        public static string GetMessageConventionsV5(string rootNamespace, string applicationName, string projectNameForInternal, string projectNameForContracts)
+        {
+            var sb = new StringBuilder();
+            if (!String.IsNullOrEmpty(rootNamespace))
+            {
+                sb.AppendLine("namespace " + rootNamespace);
+            }
+
+            sb.Append(@"{
+    public class MessageConventions : IWantToRunBeforeConfiguration
+    {
+        public void Init(Configure config)
+        {
+            config");
+            sb.AppendLine();
+            sb.AppendLine("            .DefiningCommandsAs(t => t.Namespace != null && t.Namespace.StartsWith(\"" +
+                applicationName + "." + projectNameForInternal + ".Commands\"))");
+            sb.AppendLine("            .DefiningEventsAs(t => t.Namespace != null && t.Namespace.StartsWith(\"" +
+                applicationName + "." + projectNameForContracts + "\"))");
+            sb.AppendLine("            .DefiningMessagesAs(t => t.Namespace != null && t.Namespace.StartsWith(\"" +
+                applicationName + "." + projectNameForInternal + ".Messages\"));");
+            sb.Append(@"        }
     }
 }
 ");
