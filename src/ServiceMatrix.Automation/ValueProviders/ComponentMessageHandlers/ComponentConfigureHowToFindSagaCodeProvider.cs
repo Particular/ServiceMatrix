a271c9cf8fc980e801b8f235cad4b2f8566d2600
indualagarsamy <indu.alagarsamy@gmail.com>
2014-07-02 15:18:24 -0700

    Added v4 vs v5 support for the ConfigureMapping code that gets generated.

--- src/ServiceMatrix.Automation/ValueProviders/ComponentMessageHandlers/ComponentConfigureHowToFindSagaCodeProvider.cs
+++ src/ServiceMatrix.Automation/ValueProviders/ComponentMessageHandlers/ComponentConfigureHowToFindSagaCodeProvider.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace NServiceBusStudio.Automation.ValueProviders.ComponentMessageHandlers
 {
+    using NServiceBusStudio.Automation.Model;
     using NuPattern.Runtime;
 
     [CLSCompliant(false)]
@@ -17,42 +17,63 @@ namespace NServiceBusStudio.Automation.ValueProviders.ComponentMessageHandlers
     {
         public override object Evaluate()
         {
-            return this.GenerateConfigureHowToFindSagaBody();
+            return GenerateConfigureHowToFindSagaBody();
         }
 
         private string GenerateConfigureHowToFindSagaBody()
         {
-            var targetNsbVersion = this.Component.As<IProductElement>().Root.As<IApplication>().TargetNsbVersion;
+            var targetNsbVersion = Component.As<IProductElement>().Root.As<IApplication>().TargetNsbVersion;
             //TODO: Generate appropriate mapping for NSB v4 vs NSB v5 .
             var sb = new StringBuilder();
             sb.AppendLine();
 
             var sagaStarters = new List<string>();
-            sagaStarters.AddRange(this.Component.Subscribes.ProcessedCommandLinks.Where(x => x.StartsSaga).Select(x => x.CommandReference.Value.InstanceName));
-            sagaStarters.AddRange(this.Component.Subscribes.SubscribedEventLinks.Where(x => x.StartsSaga).Select(x => x.EventReference.Value.InstanceName));
-            sagaStarters.AddRange(this.Component.Subscribes.HandledMessageLinks.Where(x => x.StartsSaga).Select(x => x.MessageReference.Value.InstanceName));
+            sagaStarters.AddRange(Component.Subscribes.ProcessedCommandLinks.Where(x => x.StartsSaga).Select(x => x.CommandReference.Value.InstanceName));
+            sagaStarters.AddRange(Component.Subscribes.SubscribedEventLinks.Where(x => x.StartsSaga).Select(x => x.EventReference.Value.InstanceName));
+            sagaStarters.AddRange(Component.Subscribes.HandledMessageLinks.Where(x => x.StartsSaga).Select(x => x.MessageReference.Value.InstanceName));
 
-            foreach (var cl in this.Component.Subscribes.ProcessedCommandLinks)
+            foreach (var cl in Component.Subscribes.ProcessedCommandLinks)
             {
                 if (sagaStarters.Any(x => x != cl.CommandReference.Value.InstanceName))
                 {
-                    sb.AppendLine("            ConfigureMapping<" + cl.CommandReference.Value.InstanceName + ">(m => /* specify message property */).ToSaga(s =>  /* specify saga property */ );");
+                    if (targetNsbVersion == TargetNsbVersion.Version4)
+                    {
+                        sb.AppendLine("            ConfigureMapping<" + cl.CommandReference.Value.InstanceName + ">(m => /* specify message property */).ToSaga(s =>  /* specify saga property */ );");
+                    }
+                    if (targetNsbVersion == TargetNsbVersion.Version5)
+                    {
+                        sb.AppendLine("            mapper.ConfigureMapping<" + cl.CommandReference.Value.InstanceName + ">(m => /* specify message property m.PropertyThatCorrelatesToSaga */).ToSaga(s => /* specify saga data property s.PropertyThatCorrelatesToMessage */);");
+                    }
                 }
             }
 
-            foreach (var el in this.Component.Subscribes.SubscribedEventLinks)
+            foreach (var el in Component.Subscribes.SubscribedEventLinks)
             {
                 if (sagaStarters.Any(x => x != el.EventReference.Value.InstanceName))
                 {
-                    sb.AppendLine("            ConfigureMapping<" + el.EventReference.Value.InstanceName + ">(m => /* specify message property */).ToSaga(s =>  /* specify saga property */ );");
+                    if (targetNsbVersion == TargetNsbVersion.Version4)
+                    {
+                        sb.AppendLine("            ConfigureMapping<" + el.EventReference.Value.InstanceName + ">(m => /* specify message property */).ToSaga(s =>  /* specify saga property */ );");
+                    }
+                    if (targetNsbVersion == TargetNsbVersion.Version5)
+                    {
+                        sb.AppendLine("            mapper.ConfigureMapping<" + el.EventReference.Value.InstanceName + ">(m => /* specify message property m.PropertyThatCorrelatesToSaga */).ToSaga(s => /* specify saga data property s.PropertyThatCorrelatesToMessage */);");
+                    }
                 }
             }
 
-            foreach (var hl in this.Component.Subscribes.HandledMessageLinks)
+            foreach (var hl in Component.Subscribes.HandledMessageLinks)
             {
                 if (sagaStarters.Any(x => x != hl.MessageReference.Value.InstanceName))
                 {
-                    sb.AppendLine("            // ConfigureMapping<" + hl.MessageReference.Value.InstanceName + ">(m => /* specify message property */).ToSaga(s =>  /* specify saga property */ );");
+                    if (targetNsbVersion == TargetNsbVersion.Version4)
+                    {
+                        sb.AppendLine("            // ConfigureMapping<" + hl.MessageReference.Value.InstanceName + ">(m => /* specify message property */).ToSaga(s =>  /* specify saga property */ );");
+                    }
+                    if (targetNsbVersion == TargetNsbVersion.Version5)
+                    {
+                        sb.AppendLine("            // mapper.ConfigureMapping<" + hl.MessageReference.Value.InstanceName + ">(m => /* specify message property m.PropertyThatCorrelatesToSaga */).ToSaga(s => /* specify saga data property s.PropertyThatCorrelatesToMessage */);");
+                    }
                 }
             }
 
