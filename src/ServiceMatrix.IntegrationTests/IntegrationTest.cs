18dc6ff4a5cefcfedf0368edfd6edeb53515d60a
fsimonazzi <fsimonazzi@hotmail.com>
2014-08-11 13:14:02 -0300

    add tests for existing solutions

--- src/ServiceMatrix.IntegrationTests/IntegrationTest.cs
+++ src/ServiceMatrix.IntegrationTests/IntegrationTest.cs
@@ -1,7 +1,20 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using EnvDTE;
+using EnvDTE80;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VSSDK.Tools.VsIdeTesting;
+using Moq;
+using NServiceBusStudio;
+using NuPattern;
+using NuPattern.Presentation;
+using NuPattern.Runtime;
+using NuPattern.VisualStudio.Solution;
 
 namespace ServiceMatrix.IntegrationTests
 {
@@ -11,11 +24,21 @@ namespace ServiceMatrix.IntegrationTests
     public abstract class IntegrationTest
     {
         private TestContext context;
+        protected ISolution solution;
+        protected IPatternManager patternManager;
+        protected DTE dte;
+
+        public virtual void InitializeContext()
+        {
+            solution = VsIdeTestHostContext.ServiceProvider.GetService<ISolution>();
+            dte = VsIdeTestHostContext.ServiceProvider.GetService<DTE>();
+            patternManager = VsIdeTestHostContext.ServiceProvider.GetService<IPatternManager>();
+        }
 
         /// <summary>
         /// Gets or sets the location of the per-test location directory.
         /// </summary>
-        public string DeploymentDirectory { get; set; }
+        protected string DeploymentDirectory { get; set; }
 
         /// <summary>
         /// Gets or sets the test context.
@@ -40,6 +63,93 @@ namespace ServiceMatrix.IntegrationTests
             get { return ((uint)context.TestName.GetHashCode()).ToString(CultureInfo.InvariantCulture); }
         }
 
+        protected static Task<bool> BuildSolutionAsync(DTE dte)
+        {
+            return Task.Factory.StartNew(() =>
+            {
+                var mre = new ManualResetEventSlim();
+                var events = dte.Events.BuildEvents;
+                _dispBuildEvents_OnBuildDoneEventHandler done = (scope, action) => mre.Set();
+                events.OnBuildDone += done;
+                try
+                {
+                    // Let build run async.
+                    dte.Solution.SolutionBuild.Build();
+
+                    // Wait until it's done.
+                    mre.Wait();
+
+                    // LastBuildInfo == # of projects that failed to build.
+                    return dte.Solution.SolutionBuild.LastBuildInfo == 0;
+                }
+                catch
+                {
+                    return false;
+                }
+                finally
+                {
+                    // Cleanup handler.
+                    events.OnBuildDone -= done;
+                }
+            }, CancellationToken.None, TaskCreationOptions.None, TaskScheduler.Default);
+        }
+
+        protected static void CloseSolution(DTE dte)
+        {
+            var mre = new ManualResetEventSlim();
+            var events = dte.Events.SolutionEvents;
+            _dispSolutionEvents_AfterClosingEventHandler done = () => mre.Set();
+            events.AfterClosing += done;
+            try
+            {
+                UIThreadInvoker.Invoke((Action)(() =>
+                {
+                    dte.Solution.Close(false);
+                    mre.Wait();
+                }));
+            }
+            finally
+            {
+                events.AfterClosing -= done;
+            }
+        }
+
+        protected static IReadOnlyCollection<string> GetBuildErrors(DTE dte)
+        {
+            var errorItems = ((DTE2)dte).ToolWindows.ErrorList.ErrorItems;
+            return Enumerable.Range(1, errorItems.Count)
+                    .Select(i =>
+                    {
+                        var errorItem = errorItems.Item(i);
+                        return string.Format(
+                            CultureInfo.CurrentCulture,
+                            "{0}({1},{2}): {3}",
+                            errorItem.FileName,
+                            errorItem.Line,
+                            errorItem.Column,
+                            errorItem.Description);
+                    })
+                    .ToList().AsReadOnly();
+        }
+
+        protected static void SetupDialogInteraction<TDialog, TViewModel>(
+            Mock<IDialogWindowFactory> factoryMock,
+            Func<TViewModel, bool?> showDialogAction)
+            where TDialog : class, IDialogWindow, new()
+            where TViewModel : class
+        {
+            TViewModel viewModel = null;
+
+            var dialogMock = new Mock<IDialogWindow>().SetupAllProperties();
+            dialogMock
+                .Setup(w => w.ShowDialog())
+                .Returns(() => showDialogAction(viewModel));
+
+            factoryMock
+                .Setup(d => d.CreateDialog<TDialog>(It.IsAny<object>()))
+                .Returns((object dc) => { viewModel = (TViewModel)dc; return dialogMock.Object; });
+        }
+
         /// <summary>
         /// Retruns a path relative to the test deployment directory.
         /// </summary>
@@ -50,7 +160,7 @@ namespace ServiceMatrix.IntegrationTests
             return Path.Combine(DeploymentDirectory, deploymentRelativePath);
         }
 
-        private void CleanDeploymentDirectory()
+        protected void CleanDeploymentDirectory()
         {
             if (Directory.Exists(DeploymentDirectory))
             {
