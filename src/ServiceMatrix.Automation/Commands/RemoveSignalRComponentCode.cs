fd2677616ec742f8cbcb0c11f182b3720f150d96
indualagarsamy <indu.alagarsamy@gmail.com>
2014-08-22 20:53:13 -0700

    Cleaned up the removal of the generated signalR files, using nupattern references instead of file artifacts from the solution.

--- src/ServiceMatrix.Automation/Commands/RemoveSignalRComponentCode.cs
+++ src/ServiceMatrix.Automation/Commands/RemoveSignalRComponentCode.cs
@@ -4,7 +4,7 @@
     using System.ComponentModel.Composition;
     using System.ComponentModel.DataAnnotations;
     using System.Linq;
-    using NServiceBusStudio.Automation.Extensions;
+    using NuPattern;
     using NuPattern.Runtime;
     using NuPattern.VisualStudio.Solution;
 
@@ -17,38 +17,36 @@
         [Import(AllowDefault = true)]
         public IProductElement CurrentElement { private get; set; }
 
+        [Required]
         [Import(AllowDefault = true)]
-        public ISolution Solution { get; set; }
-
+        private IUriReferenceService UriService { get; set; }
 
         public override void Execute()
-        {
-            // Get the associated MVC Endpoint
-            var mvcEndpoint = Model.Helpers.GetMvcEndpointFromLinkedElement(CurrentElement);
-            var component = CurrentElement.As<NServiceBusStudio.IComponent>();
-            var references = component.References;
+        {   
+            // TODO: Resolve the commandId automatically instead of passing a Guid here.
+            // For example if I obtain the command like below Name and Execute() method. How do I get the command Id??
+            // var command = CurrentElement.AutomationExtensions.First(c => c.Name.Equals("GenerateSignalRHub"));
             
-            var service = component.Parent.Parent;
+            // Delete the hub artifact reference
+            DeleteSolutionItemsInComponentForCommand("c9a984c3-c0d6-46f5-a050-6d5be1b736ab");  // --> This is the Guid for the commandId for GenerateSignalRHub command 
             
-            // Delete the hub
-            var hubPathName = String.Format("{0}\\Infrastructure\\{1}\\{2}Hub.cs", mvcEndpoint.Namespace, service.InstanceName, component.InstanceName);
-            var hubItem = Solution.FindItem(hubPathName);
-            if (hubItem != null)
-            {
-                hubItem.Delete();
-            }
-
-            // Delete the broadcasting handler
-            var broadcastHandlerPathName = String.Format("{0}\\Infrastructure\\{1}\\Broadcast{2}.cs", mvcEndpoint.Namespace, service.InstanceName, component.InstanceName);
-            var broadcastHandlerItem = Solution.FindItem(broadcastHandlerPathName);
-            if (broadcastHandlerItem != null)
-            {
-                broadcastHandlerItem.Delete();
-            }
-
+            // Delete the broadcast Handler artifact
+            DeleteSolutionItemsInComponentForCommand("c6563779-ff3e-40aa-99ac-361aa452505e"); // --> This is the Guid for the commandId for GenerateSignalRBroadcastHandler command
+            
             // Set the Broadcasting via SignalR flag on the component level to false;
             CurrentElement.As<NServiceBusStudio.IComponent>().IsBroadcastingViaSignalR = false; 
     
+        }
+
+        void DeleteSolutionItemsInComponentForCommand(string commandId)
+        {
+            var component = CurrentElement.As<NServiceBusStudio.IComponent>();
+            var artifactLink = component.References.First(t => t.Tag.Contains(commandId));
+            var solutionItem = UriService.TryResolveUri<IItemContainer>(new Uri(artifactLink.Value));
+            
+            // Delete the actual solution item and its reference in the component.
+            solutionItem.Delete();
+            artifactLink.Delete();
         }
     }
 }
